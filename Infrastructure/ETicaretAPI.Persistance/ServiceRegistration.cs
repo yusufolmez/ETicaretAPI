a44729d63be9d134bd6e78ee072848048db7d1a2
Yusuf Ölmez <yusuf.olmez670@gmail.com>
2025-03-22 13:37:20 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistance.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETicaretAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}

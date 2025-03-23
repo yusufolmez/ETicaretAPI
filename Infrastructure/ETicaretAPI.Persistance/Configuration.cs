5a6a92d09f8314a79551808a5b8b4cfcfe2d0b86
Yusuf Ölmez <yusuf.olmez670@gmail.com>
2025-03-23 12:45:32 +0300

    "Added database context and entity relationships by updating the Product class to include an Orders collection and introducing Customer and Order entities. Removed the GetProducts method from ProductService and added necessary package references for Entity Framework Core and PostgreSQL. Registered ETicaretAPIDBContext in ServiceRegistration and configured the PostgreSQL connection string in appsettings.json. Created the initial migration along with the model snapshot for future schema updates."

---
+++ Infrastructure/ETicaretAPI.Persistance/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ETicaretAPI.Persistance
+{
+    static class Configuration
+    {
+        static public string ConncetString
+        {
+            get
+            {
+                ConfigurationManager configuration = new();
+                configuration.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
+                configuration.AddJsonFile("appsettings.json");
+
+                return configuration.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistance/Contexts/ETicaretAPIDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETicaretAPI.Persistance.Contexts
+{
+    public class ETicaretAPIDBContext : DbContext
+    {
+        public ETicaretAPIDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}

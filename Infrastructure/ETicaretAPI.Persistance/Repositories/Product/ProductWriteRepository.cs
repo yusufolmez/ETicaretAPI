7654c2854a39924e657ee750284e981ad838b8c7
Yusuf Ölmez <yusuf.olmez670@gmail.com>
2025-03-23 15:05:07 +0300

    refactor(repositories): implement repository pattern and update dependencies
    Refactor repository interfaces and implementations for better modularity.
    Add specific repositories for Customer, Order, and Product entities.
    Register repository services as singletons in ServiceRegistration.
    Update ProductsController to use new repository interfaces.

---
+++ Infrastructure/ETicaretAPI.Persistance/Repositories/Product/ProductWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Persistance.Contexts;
+
+namespace ETicaretAPI.Persistance.Repositories
+{
+    public class ProductWriteRepository : WriteRepository<Product>, IProductWriteRepository
+    {
+        public ProductWriteRepository(ETicaretAPIDBContext context) : base(context)
+        {
+        }
+    }
+}

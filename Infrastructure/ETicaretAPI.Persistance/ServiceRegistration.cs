cd2ccb80903a28173130db05c2933b7675be1077
Yusuf Ölmez <yusuf.olmez670@gmail.com>
2025-03-23 12:45:32 +0300

    "Added database context and entity relationships by updating the Product class to include an Orders collection and introducing Customer and Order entities. Removed the GetProducts method from ProductService and added necessary package references for Entity Framework Core and PostgreSQL. Registered ETicaretAPIDBContext in ServiceRegistration and configured the PostgreSQL connection string in appsettings.json. Created the initial migration along with the model snapshot for future schema updates."

--- Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -1,19 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ETicaretAPI.Application.Abstractions;
-using ETicaretAPI.Persistance.Concrete;
+using ETicaretAPI.Persistance.Contexts;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace ETicaretAPI.Persistance
 {
     public static class ServiceRegistration
     {
+        
+
         public static void AddPersistanceServices(this IServiceCollection services)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETicaretAPIDBContext>(options => options.UseNpgsql(Configuration.ConncetString));
         }
     }
 }

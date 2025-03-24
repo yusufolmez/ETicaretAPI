a7619ce0828beb7a3c05163031d1fc5128dbffb5
Yusuf Ölmez <yusuf.olmez670@gmail.com>
2025-03-24 11:53:51 +0300

    "refactor(repository): improve GetByIdAsync and update service lifetimes
    Optimized `GetByIdAsync` with `Table.FindAsync`. Changed service lifetimes from `Singleton` to `Scoped`. Updated `Get` method in `ProductsController` to return `async Task` and added product retrieval by ID."

--- Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -1,31 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ETicaretAPI.Application.Abstractions;
+﻿using ETicaretAPI.Application.Repositories;
 using ETicaretAPI.Persistance.Contexts;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
-using ETicaretAPI.Application.Repositories;
 using ETicaretAPI.Persistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ETicaretAPI.Persistance
 {
     public static class ServiceRegistration
     {
-        
+
 
         public static void AddPersistanceServices(this IServiceCollection services)
         {
             services.AddDbContext<ETicaretAPIDBContext>(options => options.UseNpgsql(Configuration.ConncetString), ServiceLifetime.Singleton);
-            services.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
-            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
-            services.AddSingleton<IProductReadReadRepository, ProductReadRepository>();
-            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
-            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
-            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IProductReadReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
         }
     }
 }

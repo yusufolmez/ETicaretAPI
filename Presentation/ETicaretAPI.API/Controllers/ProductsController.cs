fa99b4f122c9c4f092698c5468f72749fe2886c1
Yusuf Ölmez <yusuf.olmez670@gmail.com>
2025-03-25 14:37:26 +0300

    feat: add pagination to ProductsController Get method
    - Introduced a `Pagination` class to handle pagination parameters (`Page` and `Size`).
    - Updated `Get` method to accept a `Pagination` object for customizable paging.
    - Implemented logic to calculate the total product count.
    - Modified response to include both the total count and the paginated product list.

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using ETicaretAPI.Application.Abstractions;
 using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Application.RequestParameters;
 using ETicaretAPI.Application.ViewModels.Products;
 using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
@@ -23,9 +24,20 @@ namespace ETicaretAPI.API.Controllers
             _productWriteRepository = productWriteRepository;
         }
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery]Pagination pagination)
         {
-            return Ok(_productReadRepository.GetAll(false));
+            var totalCount = _productReadRepository.GetAll(false).Count();
+            var products = _productReadRepository.GetAll(false).Skip(pagination.Page * pagination.Size).Take(pagination.Size).Select(p => new
+            {
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedDate,
+                p.UpdatedDate
+            }).ToList();
+
+            return Ok(new {totalCount,products});
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
